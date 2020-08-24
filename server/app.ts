c39de44a7feb03a39bbb29d680c071a2dd932b83
Taiga Tachibana <tachiba@gmail.com>
2020-08-24 23:39:38 +0900

    add allowed origin

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
+import * as cors from "cors";
 import { registerRoutes } from "app/controller/router";
 import { errorHandler } from "app/controller/errorHandler";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-registerRoutes(app)
-app.use(errorHandler)
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+registerRoutes(app);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
