007a7abf5291a34da6e42fa31622d5d017308ce5
Taiga Tachibana <tachiba@gmail.com>
2020-08-27 22:56:07 +0900

    add helper method to extract youtube video id

--- client/modules/video/helpers.ts
+++ client/modules/video/helpers.ts
@@ -4,3 +4,17 @@ export const formatCaptionTime = (time: number): string => {
   const sec = Math.floor(rounded % 60)
   return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
 }
+
+export const extractYoutubeVideoId = (text: string): string | null => {
+  const fullRegex = /^[^#&?]{11}$/
+  const fullMatch = text.match(fullRegex)
+  if (fullMatch) {
+    return text
+  }
+  const urlRegex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]{11}).*/
+  const urlMatch = text.match(urlRegex)
+  if (urlMatch) {
+    return urlMatch[2]
+  }
+  return null
+}
