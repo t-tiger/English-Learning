7756f45e53d8beb1485adce0f91a92a1fbff17ad
Taiga Tachibana <tachiba@gmail.com>
2020-08-31 22:10:22 +0900

    add monorepo for yarn

--- client/modules/video/api.ts
+++ client/modules/video/api.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
-import { VideoDetail } from 'modules/video/types'
+import { VideoDetail, VideoOutline } from 'modules/video/types'
 import { API_ENDPOINT } from 'const/index'
+
+export const fetchVideos = async (): Promise<{ videos: VideoOutline[] }> => {
+  const { data } = await axios.get(`${API_ENDPOINT}/videos`)
+  return data
+}
 
 export const fetchVideoDetail = async (id: string): Promise<VideoDetail> => {
   const { data } = await axios.get(`${API_ENDPOINT}/videos/${id}`)
